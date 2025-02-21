72ae80fb510a050c0bf2e6e336abc0175f45c061
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-02-21 14:20:51 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,6 +22,7 @@ NODE_CLASS_MAPPINGS = {
     "Masked Diff C2E": C2EMaskedDiffNode,
     "Crop Image with Coords": CropImageWithCoordsNode,
     "Paste Image with Coords": PasteImageWithCoordsNode,
+    "Pad 180 to 360 Equirectangular": Pad180To360Node,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -35,4 +36,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Masked Diff C2E": "Masked Diff C2E",
     "Crop Image with Coords": "Crop Image with Coords",
     "Paste Image with Coords": "Paste Image with Coords",
+    ""Pad 180 to 360 Equirectangular": "Pad 180 to 360 Equirectangular",
 }
