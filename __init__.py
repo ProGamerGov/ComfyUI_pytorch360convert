8d55c02c3151323154601e3e6ff68b03ee798cbe
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-02-27 11:27:00 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 from .nodes import (
     C2EMaskedDiffNode,
     C2ENode,
+    Crop360To180Node,
     CropImageWithCoordsNode,
     E2CNode,
     E2ENode,
     E2PNode,
+    MonoScopicToStereo,
+    Pad180To360Node,
     PasteImageWithCoordsNode,
     RollImageNode,
     SplitFacesNode,
     StackFacesNode,
+    StereoToMonoScopic,
 )
 
 NODE_CLASS_MAPPINGS = {
@@ -23,6 +27,10 @@ NODE_CLASS_MAPPINGS = {
     "Crop Image with Coords": CropImageWithCoordsNode,
     "Paste Image with Coords": PasteImageWithCoordsNode,
     "Pad 180 to 360 Equirectangular": Pad180To360Node,
+    "Crop 360 to 180 Equirectangular": Crop360To180Node,
+    "Crop Stereo to Monoscopic": StereoToMonoScopic,
+    "Merge Monoscopic into Stereo": MonoScopicToStereo,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -37,4 +45,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Crop Image with Coords": "Crop Image with Coords",
     "Paste Image with Coords": "Paste Image with Coords",
     "Pad 180 to 360 Equirectangular": "Pad 180 to 360 Equirectangular",
+    "Crop 360 to 180 Equirectangular": "Crop 360 to 180 Equirectangular",
+    "Crop Stereo to Monoscopic": "Crop Stereo to Monoscopic",
+    "Merge Monoscopic into Stereo": "Merge Monoscopic into Stereo",
 }
