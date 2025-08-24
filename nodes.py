aa955f7b5ebb270ae2a52e26b40a2d41574439ca
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-08-24 12:03:58 -0600

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1469,3 +1469,32 @@ class MaskE2ENode:
                 channels_first=False,
             )[..., 0],
         )
+
+
+class Create180To360Mask:
+    @classmethod
+    def INPUT_TYPES(s) -> Dict:
+        return {
+            "required": {
+                "image": ("IMAGE", {"default": None}),
+            },
+        }
+
+    RETURN_TYPES = ("MASK",)
+    RETURN_NAMES = ("Equirectangular Mask",)
+
+    FUNCTION = "mask_180_to_360"
+
+    CATEGORY = "pytorch360convert/mask"
+
+    def mask_180_to_360(self, image: torch.Tensor) -> Tuple[torch.Tensor]:
+        assert image.dim() == 4, f"image should have 4 dimensions, got {image.dim()}"
+        _, H, W, _ = image.shape
+        pad_left = W // 2
+        pad_right = W - pad_left
+
+        mask = torch.zeros(1, 1, H, W, dtype=image.dtype, device=image.device)  
+        mask_padded = torch.nn.functional.pad(
+            mask, (pad_left, pad_right), mode="constant", value=0.0
+        )
+        return (mask_padded[:, 0, ...],)
